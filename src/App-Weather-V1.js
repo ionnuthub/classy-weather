b3ce1e5a416bb0468b717f14da63f0094f8b6b89
ionnuthub <raducugionut@gmail.com>
2024-02-28 21:48:48 +0100

    Initial Commit

--- src/App-Weather-V1.js
+++ src/App-Weather-V1.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Weather from "./Weather";
-import Input from "./Input";
 
 function getWeatherIcon(wmoCode) {
   const icons = new Map([
@@ -35,15 +34,18 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
-  state = {
-    location: "",
-    isLoading: false,
-    dispalyLocation: "",
-    weather: {},
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "lisbon",
+      isLoading: false,
+      dispalyLocation: "",
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
 
-  fetchWeather = async () => {
-    if (this.state.location.length < 2) return this.setState({ weather: {} });
+  async fetchWeather() {
     try {
       this.setState({ isLoading: true });
 
@@ -69,26 +71,9 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.error(err);
+      console.err(err);
     } finally {
       this.setState({ isLoading: false });
-    }
-  };
-
-  setLocation = (e) => this.setState({ location: e.target.value });
-
-  //It is like useEffect [] runs just on mount
-  componentDidMount() {
-    // this.fetchWeather();
-    this.setState({ location: localStorage.getItem("location" || "") });
-  }
-
-  // It's like useEffect [location ] in the dependencie array. Its called only on rerenders
-  componentDidUpdate(prevProps, prevState) {
-    //we search for the weather as we type
-    if (this.state.location !== prevState.location) {
-      this.fetchWeather();
-      localStorage.setItem("location", this.state.location);
     }
   }
 
@@ -96,11 +81,15 @@ class App extends React.Component {
     return (
       <div className="app">
         <h1>Classy Wheater</h1>
-        <Input
-          location={this.state.location}
-          onChangeLocation={this.setLocation}
-        />
-
+        <div>
+          <input
+            type="text"
+            placeholder="Search for location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
         {this.state.weather.weathercode && (
